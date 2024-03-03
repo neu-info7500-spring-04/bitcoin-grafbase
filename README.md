00f9f681299b9de6263e9d8e686f53fad25d2e3c
Suhabe Bugrara <suhabe@bugrara.com>
2024-03-02 16:35:41 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Bitcoin Grafbase
+
+* Install the latest LTS version of Nodejs.
+* Clone this repository: `git clone git@github.com:neu-info7500-spring-04/bitcoin-grafbase.git`
+* Copy the `.env.example` file and name it `.env`. 
+* Update your `.env` file with your own API keys for the endpoints.
+* In the `bitcoin-grafbase` directory, execute `npm grafbase dev`
+
+The gateway should be at http://127.0.0.1:4000/graphql.
+
+Use Postman to try the following query on the gateway.
+This query invokes the bitquery subgraph. 
+```graphql
+query {
+    bitquery {
+        bitcoin {
+            blocks {
+                count
+            }
+        }
+    }
+}
+```
+The postman view should look like:
+![postman-example.png](postman-example.png)
+
+
+
---
+++ grafbase.config.ts
@@ -0,0 +1,49 @@
+import { graph, connector, config } from '@grafbase/sdk'
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+// Welcome to Grafbase!
+//
+// Configure authentication, data sources, resolvers and caching for your GraphQL API.
+
+const g = graph.Standalone();
+
+const contentful = connector.GraphQL('bitquery', {
+  url: "https://graphql.bitquery.io",
+  headers: headers => {
+    headers.set('X-API-KEY', g.env("BITQUERY_API_KEY"))
+  },
+})
+
+g.datasource(contentful)
+
+const coinapi = connector.OpenAPI('coinapi', {
+  schema: 'https://raw.githubusercontent.com/coinapi/coinapi-sdk/master/data-api/coinapi-marketdata-rest.yaml',
+  headers: headers => {
+    headers.set('X-CoinAPI-Key', g.env("COINAPI_API_KEY"))
+  },
+})
+
+g.datasource(coinapi)
+export default config({
+  graph: g,
+  // Authentication - https://grafbase.com/docs/auth
+  auth: {
+    // OpenID Connect
+    // const oidc = auth.OpenIDConnect({ issuer: g.env('OIDC_ISSUER_URL') })
+    // providers: [oidc],
+    rules: (rules) => {
+      rules.public()
+    },
+  },
+  // Caching - https://grafbase.com/docs/graphql-edge-caching
+  // cache: {
+  //   rules: [
+  //     {
+  //       types: ['Query'], // Cache everything for 60 seconds
+  //       maxAge: 60,
+  //       staleWhileRevalidate: 60
+  //     }
+  //   ]
+  // }
+})
