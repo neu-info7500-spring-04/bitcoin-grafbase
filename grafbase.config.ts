4a8333008efdb687c26339a857b301d0f04a8bca
Rakshith-Rajkumar-NEU <144841914+Rakshith-Rajkumar@users.noreply.github.com>
2024-03-15 15:08:11 -0400

    Update grafbase.config.ts

--- grafbase.config.ts
+++ grafbase.config.ts
@@ -1,5 +1,5 @@
-import { graph, connector, config } from '@grafbase/sdk'
-import * as dotenv from 'dotenv';
+import { graph, connector, config } from "@grafbase/sdk";
+import * as dotenv from "dotenv";
 dotenv.config();
 
 // Welcome to Grafbase!
@@ -8,23 +8,34 @@ dotenv.config();
 
 const g = graph.Standalone();
 
-const contentful = connector.GraphQL('bitquery', {
+const contentful = connector.GraphQL("bitquery", {
   url: "https://graphql.bitquery.io",
-  headers: headers => {
-    headers.set('X-API-KEY', g.env("BITQUERY_API_KEY"))
+  headers: (headers) => {
+    headers.set("X-API-KEY", g.env("BITQUERY_API_KEY"));
   },
-})
+});
 
-g.datasource(contentful)
+g.datasource(contentful);
 
-const coinapi = connector.OpenAPI('coinapi', {
-  schema: 'https://raw.githubusercontent.com/coinapi/coinapi-sdk/master/data-api/coinapi-marketdata-rest.yaml',
-  headers: headers => {
-    headers.set('X-CoinAPI-Key', g.env("COINAPI_API_KEY"))
+const coinapi = connector.OpenAPI("coinapi", {
+  schema:
+    "https://raw.githubusercontent.com/coinapi/coinapi-sdk/master/data-api/coinapi-marketdata-rest.yaml",
+  headers: (headers) => {
+    headers.set("X-CoinAPI-Key", g.env("COINAPI_API_KEY"));
   },
-})
+});
 
-g.datasource(coinapi)
+g.datasource(coinapi);
+
+//Configuring bitnodes connectors
+const bitnode = connector.OpenAPI("bitnodes", {
+  schema:
+    "https://raw.githubusercontent.com/neu-info7500-spring-04/Bitnodes-API-Schema/e46f130cec12627f6a5f9d55053e4098736ec3e3/Bitnodes-Snapshot.yml",
+  url: "https://bitnodes.io/api/v1/",
+});
+
+g.datasource(bitnode);
+
 export default config({
   graph: g,
   // Authentication - https://grafbase.com/docs/auth
@@ -33,7 +44,7 @@ export default config({
     // const oidc = auth.OpenIDConnect({ issuer: g.env('OIDC_ISSUER_URL') })
     // providers: [oidc],
     rules: (rules) => {
-      rules.public()
+      rules.public();
     },
   },
   // Caching - https://grafbase.com/docs/graphql-edge-caching
@@ -46,4 +57,4 @@ export default config({
   //     }
   //   ]
   // }
-})
+});
