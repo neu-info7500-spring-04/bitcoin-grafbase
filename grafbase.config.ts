e6ce5032e2a71b6b3519fe470da942c9db9366f7
Srinivasa Reddy Akumalla <113478192+akumalla-s@users.noreply.github.com>
2024-03-17 18:11:01 -0400

    openAPI specification

--- grafbase.config.ts
+++ grafbase.config.ts
@@ -8,6 +8,20 @@ dotenv.config();
 
 const g = graph.Standalone();
 
+const blockstream = connector.OpenAPI('blockstream', {
+  schema: 'https://raw.githubusercontent.com/dheeru99/liquid_transactions/main/transactions/liquid_transactions_grafbase/openAPI.yaml',
+  url: "https://blockstream.info/liquid/api/",
+});
+
+g.datasource(blockstream);
+
+const mempool = connector.OpenAPI('mempool', {
+  schema: 'https://raw.githubusercontent.com/akumalla-s/',
+  url: "https://mempool.space/api/",
+});
+
+g.datasource(mempool);
+
 const contentful = connector.GraphQL('bitquery', {
   url: "https://graphql.bitquery.io",
   headers: headers => {
