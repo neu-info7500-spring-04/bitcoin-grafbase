9ad2e398b06e52675842eef1b6bc27318e7dd92a
Srinivasa Reddy Akumalla <113478192+akumalla-s@users.noreply.github.com>
2024-03-18 12:29:36 -0400

    OpenAPI schema for mempool
    OpenAPI schema for mempool to get the last 24h miner distribution pool data

--- grafbase.config.ts
+++ grafbase.config.ts
@@ -8,16 +8,9 @@ dotenv.config();
 
 const g = graph.Standalone();
 
-const blockstream = connector.OpenAPI('blockstream', {
-  schema: 'https://raw.githubusercontent.com/dheeru99/liquid_transactions/main/transactions/liquid_transactions_grafbase/openAPI.yaml',
-  url: "https://blockstream.info/liquid/api/",
-});
-
-g.datasource(blockstream);
-
 const mempool = connector.OpenAPI('mempool', {
-  schema: 'https://raw.githubusercontent.com/akumalla-s/',
-  url: "https://mempool.space/api/",
+  schema: 'https://raw.githubusercontent.com/akumalla-s/mempoolspecification/main/openAPI.yaml',
+  url: "https://mempool.space/api/v1",
 });
 
 g.datasource(mempool);
