aca8894f4d879731adca4796825f181dbc4d23d9
Rakshith Rajkumar NEU <rajkumar.rak@northeastern.edu>
2024-03-19 13:56:41 -0400

    Update grafbase.config.ts (#4)
    Co-authored-by: Rakshith-Rajkumar-NEU <58843287+rakshithvk19@users.noreply.github.com>

--- grafbase.config.ts
+++ grafbase.config.ts
@@ -1,5 +1,5 @@
-import { graph, connector, config } from '@grafbase/sdk'
-import * as dotenv from 'dotenv';
+import { graph, connector, config } from "@grafbase/sdk";
+import * as dotenv from "dotenv";
 dotenv.config();
 
 // Welcome to Grafbase!
@@ -8,30 +8,42 @@ dotenv.config();
 
 const g = graph.Standalone();
 
-const mempool = connector.OpenAPI('mempool', {
-  schema: 'https://raw.githubusercontent.com/neu-info7500-spring-04/bitcoin-grafbase/main/openAPI.yaml',
+const mempool = connector.OpenAPI("mempool", {
+  schema:
+    "https://raw.githubusercontent.com/neu-info7500-spring-04/bitcoin-grafbase/main/openAPI.yaml",
   url: "https://mempool.space/api/v1",
 });
 
 g.datasource(mempool);
 
-const contentful = connector.GraphQL('bitquery', {
+const contentful = connector.GraphQL("bitquery", {
   url: "https://graphql.bitquery.io",
-  headers: headers => {
-    headers.set('X-API-KEY', g.env("BITQUERY_API_KEY"))
+  headers: (headers) => {
+    headers.set("X-API-KEY", g.env("BITQUERY_API_KEY"));
   },
-})
+});
 
-g.datasource(contentful)
+g.datasource(contentful);
 
-const coinapi = connector.OpenAPI('coinapi', {
-  schema: 'https://raw.githubusercontent.com/coinapi/coinapi-sdk/master/data-api/coinapi-marketdata-rest.yaml',
-  headers: headers => {
-    headers.set('X-CoinAPI-Key', g.env("COINAPI_API_KEY"))
+const coinapi = connector.OpenAPI("coinapi", {
+  schema:
+    "https://raw.githubusercontent.com/coinapi/coinapi-sdk/master/data-api/coinapi-marketdata-rest.yaml",
+  headers: (headers) => {
+    headers.set("X-CoinAPI-Key", g.env("COINAPI_API_KEY"));
   },
-})
+});
 
-g.datasource(coinapi)
+g.datasource(coinapi);
+
+//Configuring bitnodes connectors
+const bitnode = connector.OpenAPI("bitnodes", {
+  schema:
+    "https://raw.githubusercontent.com/neu-info7500-spring-04/Bitnodes-API-Schema/e46f130cec12627f6a5f9d55053e4098736ec3e3/Bitnodes-Snapshot.yml",
+  url: "https://bitnodes.io/api/v1/",
+});
+
+g.datasource(bitnode);
+
 export default config({
   graph: g,
   // Authentication - https://grafbase.com/docs/auth
@@ -40,7 +52,7 @@ export default config({
     // const oidc = auth.OpenIDConnect({ issuer: g.env('OIDC_ISSUER_URL') })
     // providers: [oidc],
     rules: (rules) => {
-      rules.public()
+      rules.public();
     },
   },
   // Caching - https://grafbase.com/docs/graphql-edge-caching
@@ -53,4 +65,4 @@ export default config({
   //     }
   //   ]
   // }
-})
+});
