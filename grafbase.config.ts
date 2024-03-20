aa3b6810b839161e36706c5799f1d8d7fbfc1d9e
Shaiz Akhtar <mohammad.sh@northeastern.edu>
2024-03-20 14:33:51 -0400

    uncommenting crypto api

--- grafbase.config.ts
+++ grafbase.config.ts
@@ -69,15 +69,15 @@ const blockChainApi = connector.OpenAPI('blockchainapi',{
 });
 g.datasource(blockChainApi)
 
-// const cryptoapis = connector.OpenAPI('cryptoapis', {
-//   schema: 'https://raw.githubusercontent.com/Romil-Tiwari1/Config-Spec-Yaml/main/cryptoapis_spec.yaml', // You need to provide an OpenAPI spec for your CryptoAPIs endpoint.
-//   url: "https://rest.cryptoapis.io/v2",
-//   headers: headers => {
-//     headers.set('X-API-Key', g.env("CRYPTOAPIS_API_KEY")) // Make sure to replace with your actual API key name
-//   },
-// })
+const cryptoapis = connector.OpenAPI('cryptoapis', {
+  schema: 'https://raw.githubusercontent.com/Romil-Tiwari1/Config-Spec-Yaml/main/cryptoapis_spec.yaml', // You need to provide an OpenAPI spec for your CryptoAPIs endpoint.
+  url: "https://rest.cryptoapis.io/v2",
+  headers: headers => {
+    headers.set('X-API-Key', g.env("CRYPTOAPIS_API_KEY")) // Make sure to replace with your actual API key name
+  },
+})
 
-//g.datasource(cryptoapis)
+g.datasource(cryptoapis)
 
 export default config({
   graph: g,
